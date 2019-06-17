d2df1fec437da425eff628e29388e9bab249503b
Eufronio <eufroniowoori@gmail.com>
2019-06-17 12:05:59 -0700

    Initial commit

---
+++ src/components/PrimerComponente.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class PrimerComponete extends Component {
+   
+    render() { 
+        return ( <h1>Hola Mundo </h1>  );
+    }
+}
+ 
+export default PrimerComponete;
